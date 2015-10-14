a2b1a345841eb093ffc43a2d673cc7e6a163bbe9
Luke <lukealization@gmail.com>
2015-10-15 08:39:46 +1300

    rewrite to use different classes when method chaining

--- src/Exceptions/SubredditContextException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace LukeNZ\Reddit\Exceptions;
-
-class SubredditContextException extends \Exception {
-    protected $message = "Please set a subreddit using the 'subreddit()' method before calling methods dependent on a subreddit context";
-}
