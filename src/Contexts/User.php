8472f2bd670cc0a77e0417b2a28f2423aa6f634e
Lukas <lukealization@gmail.com>
2015-10-25 11:19:24 +1300

    context changes

--- src/Contexts/User.php
+++ src/Contexts/User.php
@@ -9,17 +9,9 @@ class User {
 
 	protected $client, $user;
 
-	public function __construct($client, $username, $otherContexts) {
+	public function __construct($client, $username) {
 		$this->client = $client;
-		$this->user = $username;
-		
-		if (array_key_exists('subreddit', $otherContexts)) {
-			$this->subreddit = $otherContexts['subreddit'];
-		}
-
-		if (array_key_exists('thing', $otherContexts)) {
-			$this->thing = $otherContexts['thing'];
-		}
+		$this->client->userContext = $username;
 	}
 
 	public function setFlair(array $options) {		
