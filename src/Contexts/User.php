3b078dacad46ef678830eac2ba1784ba59d57ee5
Lukas <lukealization@gmail.com>
2015-11-14 19:30:35 +1300

    user submitted endpoint

--- src/Contexts/User.php
+++ src/Contexts/User.php
@@ -14,6 +14,39 @@ class User {
 		$this->client->userContext = $username;
 	}
 
+    /**
+     * Fetch the submitted selfposts and links for the user in the current context.
+     *
+     * @param $sort
+     * @param $timeInterval
+     * @param $afterThing
+     * @param $beforeThing
+     * @param $count
+     * @param $limit
+     * @param bool|false $subredditDetail
+     * @return mixed
+     */
+    public function submitted($sort, $timeInterval, $afterThing, $beforeThing, $count, $limit, $subredditDetail = false) {
+        $options['show'] = 'given';
+        $options['sort'] = $sort;
+        $options['t'] = $timeInterval;
+        $options['username'] = $this->client->userContext;
+        $options['after'] = $afterThing;
+        $options['before'] = $beforeThing;
+        $options['count'] = $count;
+
+        if (!is_null($limit)) {
+            $options['limit'] = min($options['count'], 100);
+        } else {
+            $options['limit'] = 25;
+        }
+
+        $options['sr_detail'] = $subredditDetail;
+
+        $response = $this->client->httpRequest(HttpMethod::POST, "api/{$this->client->userContext}/submitted", $options);
+        return $response->getBody();
+    }
+
 	public function setFlair(array $options) {		
 	}
 
