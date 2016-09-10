352caf79bb8fe2f9f7fe936c919504c054aac9d4
Bob Kruithof <bobkruithof@gmail.com>
2016-09-10 21:34:38 +0200

    - Re-generated the API documentation after installing graphviz on my macbook. There is a .svg file now :)

---
+++ test/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'username'	=> 'coinflipbot',
+	'password'	=> 'tijdelijk',
+	'id'		=> 'JAoOFzVJHrw4zQ',
+	'secret'	=> '54D8KIeNwFnG3Su-eaUE7OlCkM8',
+	'userAgent'	=> 'Coinflipbot by /u/supersniperguy',
+];
