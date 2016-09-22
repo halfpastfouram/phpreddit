730ce34a27360e31567f00a83c157c8865423ff3
Bob <bobkruithof@gmail.com>
2016-09-22 22:43:25 +0200

    Removed config file from test directory.

--- test/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	'username'	=> 'coinflipbot',
-	'password'	=> 'tijdelijk',
-	'id'		=> 'JAoOFzVJHrw4zQ',
-	'secret'	=> '54D8KIeNwFnG3Su-eaUE7OlCkM8',
-	'userAgent'	=> 'Coinflipbot by /u/supersniperguy',
-];
